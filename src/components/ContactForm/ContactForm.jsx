de2d0dc1d9e1f72bb09313228f4cd46faedea814
Sanjana <ghelotiasanjana2003@gmail.com>
2026-05-19 11:41:50 +0530

    changes in UI

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -297,9 +297,8 @@ function ContactForm() {
 
                 {/* SHIPPING METHOD */}
                 <div
-                  className={`form-group form-group-full select-group ${
-                    shippingOpen ? "select-open" : ""
-                  }`}
+                  className={`form-group form-group-full select-group ${shippingOpen ? "select-open" : ""
+                    }`}
                 >
                   <select
                     name="shippingMethod"
